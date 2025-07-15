5aa052e70aca92f07716b18a1d4b20f1f6a3a15c
Esra Ayaz <essayaz@hotmail.com>
2025-07-15 21:08:58 +0200

    Old codes have been transferred 🔋

---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <div className="signature">
+      <p>
+        Coded by
+        <a
+          id="signature"
+          href="https://www.linkedin.com/in/esraayaz/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Esra Ayaz
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
