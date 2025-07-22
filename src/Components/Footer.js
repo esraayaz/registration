149b63851eedc1711e859ebc593beccc4cce45ab
Esra Ayaz <essayaz@hotmail.com>
2025-07-22 18:09:53 +0200

    GitHub Repo link added 🌼

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,15 +2,22 @@ function Footer() {
   return (
     <div className="signature">
       <p>
-        Coded by
+        Coded by{" "}
         <a
-          id="signature"
           href="https://www.linkedin.com/in/esraayaz/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          {" "}
           Esra Ayaz
+        </a>
+        <span> | </span>
+        <a
+          id="repo"
+          href="https://github.com/esraayaz/calculator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
         </a>
       </p>
     </div>
